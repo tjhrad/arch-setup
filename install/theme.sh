ee8c9aa94b7ab6d96d82cfa1153b03eaccb4db8e
tjhrad <14080006+tjhrad@users.noreply.github.com>
2025-11-05 20:03:17 -0700

    Fix kwriteconfig version

--- install/theme.sh
+++ install/theme.sh
@@ -3,9 +3,9 @@ echo "Applying Breeze Dark theme"
 lookandfeeltool -a org.kde.breezedark.desktop
 
 # Ensure application style & decorations
-kwriteconfig5 --file kdeglobals --group KDE --key widgetStyle "breeze"
-kwriteconfig5 --file kwinrc --group org.kde.kdecoration2 --key Library "org.kde.breezedecoration"
-kwriteconfig5 --file kwinrc --group org.kde.kdecoration2 --key Theme "org.kde.breezedecoration"
+kwriteconfig6 --file kdeglobals --group KDE --key widgetStyle "breeze"
+kwriteconfig6 --file kwinrc --group org.kde.kdecoration2 --key Library "org.kde.breezedecoration"
+kwriteconfig6 --file kwinrc --group org.kde.kdecoration2 --key Theme "org.kde.breezedecoration"
 
 # Refresh KWin to apply
 qdbus org.kde.KWin /KWin reconfigure
